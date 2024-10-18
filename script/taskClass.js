8c9712036dc451777926e398e89374d01e7db419
Felix <felavalos@gmail.com>
2024-10-18 11:10:23 -0600

    task cosntructor

---
+++ script/taskClass.js
@@ -0,0 +1,14 @@
+class Task {
+  id;
+  name;
+  content = document.getElementById('content');
+
+  constructor( id, name ) {
+    this.id = id;
+    this.name = name;
+  }
+
+  createTaskContainer = () => {
+    
+  }
+}
