d34a91d39c1b4bfcb090db4dbef2c839ebc10135
Felix <felavalos@gmail.com>
2024-10-18 12:12:49 -0600

    Delete task function

--- script/script.js
+++ script/script.js
@@ -3,11 +3,16 @@ import { Task } from "./taskClass.js";
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
 let taskCounter = 0;
+let tasksList = [];
+
+const deleteTask = (id) => {
+  console.log('Removed ', id);
+}
 
 addButton.addEventListener('click', () => {
   const taskName = taskInput.value.toLowerCase();
 
-  new Task(taskCounter, taskName);
+  new Task(taskCounter, taskName, deleteTask);
 
   taskCounter++;
 });
