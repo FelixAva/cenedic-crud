6ef1db19c60789ea7c4a684c1dfc6c0f6f902208
Felix <felavalos@gmail.com>
2024-10-18 12:15:31 -0600

    Create task function

--- script/script.js
+++ script/script.js
@@ -5,14 +5,18 @@ const addButton = document.getElementById('addButton');
 let taskCounter = 0;
 let tasksList = [];
 
+const createTask = (taskName) => {
+  return new Task(taskCounter, taskName, deleteTask);
+};
+
 const deleteTask = (id) => {
   console.log('Removed ', id);
-}
+};
 
 addButton.addEventListener('click', () => {
   const taskName = taskInput.value.toLowerCase();
 
-  new Task(taskCounter, taskName, deleteTask);
+  createTask(taskName);
 
   taskCounter++;
 });
