63af92366518583d9beca7b0c09b7323747e07f9
Felix <felavalos@gmail.com>
2024-10-26 20:47:07 -0600

    Create helpers to manage localstorage data

---
+++ utils/localStorage.js
@@ -0,0 +1,13 @@
+const saveToLocalStorage = ( key, item ) => {
+  localStorage.setItem(key, JSON.stringify( item ));
+};
+
+const getFromLocalStorage = ( key ) => {
+  const result = localStorage.getItem( key );
+
+  return result ? JSON.parse( result ) : null;
+};
+
+const removeFromLocalStorage = ( key ) => {
+  localStorage.removeItem( key );
+};
